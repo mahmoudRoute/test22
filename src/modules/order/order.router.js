9502b0e63f98b3ea7b8ff32a82db10924522d0cb
Mahmoud Elwan <mahmoudelwan460@gmail.com>
2023-09-22 03:18:20 +0300

    add

---
+++ src/modules/order/order.router.js
@@ -0,0 +1,15 @@
+import * as orderController from './controller/order.js'
+import * as validators from './order.validation.js'
+import express from 'express'
+import { validation } from '../../middleware/validation.js';
+
+import { endpoint } from './order.endPoint.js'
+import { auth } from '../../middleware/auth.js'
+import { Router } from "express";
+const router = Router()
+
+
+
+
+
+export default router
